7eeb815c56f7f9d0167a881699b410f5064b3874
Haja Florin-Gabriel <haja.fgabriel@gmail.com>
2021-05-06 23:37:37 +0300

    Made Observer class abstract by checking if it's instantiated directly

--- src/utils/observer/observer.js
+++ src/utils/observer/observer.js
@@ -1,5 +1,8 @@
 export class Observer {
     constructor(uuid) {
+        if (new.target === Observer) {
+            throw new TypeError("Must not instantiate directly");
+        }
         this._uuid = uuid;
         if (this.notify === undefined) {
             throw new TypeError("Must override the observer notification");
