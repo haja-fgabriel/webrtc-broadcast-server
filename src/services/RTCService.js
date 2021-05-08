324533f1ea3ee8b1824da75ed031d05f1c1df89b
Haja Florin-Gabriel <haja.fgabriel@gmail.com>
2021-05-08 13:34:09 +0300

    Added suggested ESLint fixes

--- src/services/RTCService.js
+++ src/services/RTCService.js
@@ -1,78 +1,75 @@
-import { RTCClientNode, RTCRoom } from "../models";
-import { RTCRoomInMemoryRepository } from "../repositories";
-import { Observable } from "../utils";
+import { RTCRoom } from '../models'
+import { Observable } from '../utils'
 
 /**
  * Service that handles the business logic of connecting clients and
  * handling call rooms.
  */
 export class RTCService extends Observable {
-    /**
-     * @param {RTCRoomInMemoryRepository} roomRepository 
+  /**
+     * @param {RTCRoomInMemoryRepository} roomRepository
      */
-    constructor(roomRepository) {
-        super();
-        this.roomRepository = roomRepository;
-    }
+  constructor (roomRepository) {
+    super()
+    this.roomRepository = roomRepository
+  }
 
-    /**
+  /**
      * Adds a client to a given room.
-     * @param {string} roomName 
-     * @param {*} client 
+     * @param {string} roomName
+     * @param {*} client
      * @returns {Promise<void | Error>}
      */
-    async joinRoom(roomName, client) {
-        var room = this.roomRepository.get(roomName);
-        
-        if (this._observers.get(client) === undefined) {
-            return Promise.reject(
-                new Error(`Please add the client with ID  ${client} to the`
-                    + ' observers list.'));
-        }
+  async joinRoom (roomName, client) {
+    let room = this.roomRepository.get(roomName)
 
-        if (room === undefined) {
-            room = new RTCRoom(roomName);
-            this.roomRepository.add(room);            
-        } 
-        
-        if (room.broadcasterID) {
-            // adding a viewer
-            room.addClient(client);
-            super.notify(client, '[response]rtc:joining-as-viewer', undefined);
-            let parent = room.getParentForClient(client);
-            if (parent) {
-                // TODO maybe add more args
-                super.notify(parent, '[webrtc]make-offer', client);
-            }
-        } else {
-            room.addClient(client);
-
-            // TODO add parameters too
-            super.notify(
-                client, '[response]rtc:joining-as-broadcaster', undefined);
-        }
-        
-        // TODO notify observer
-        return Promise.resolve();
+    if (this._observers.get(client) === undefined) {
+      return Promise.reject(
+        new Error(`Please add the client with ID  ${client} to the` +
+                    ' observers list.'))
     }
 
-    /**
+    if (room === undefined) {
+      room = new RTCRoom(roomName)
+      this.roomRepository.add(room)
+    }
+
+    if (room.broadcasterID) {
+      // adding a viewer
+      room.addClient(client)
+      super.notify(client, '[response]rtc:joining-as-viewer', undefined)
+      const parent = room.getParentForClient(client)
+      if (parent) {
+        // TODO maybe add more args
+        super.notify(parent, '[webrtc]make-offer', client)
+      }
+    } else {
+      room.addClient(client)
+
+      // TODO add parameters too
+      super.notify(
+        client, '[response]rtc:joining-as-broadcaster', undefined)
+    }
+
+    // TODO notify observer
+    return Promise.resolve()
+  }
+
+  /**
      * Returns a list of clients for the given room.
      * @returns {RTCClientNode[]}
      */
-    getClientsForRoom(roomName) {
-        var room = this.roomRepository.get(roomName);
-        return Array.from(room.clients.values());
-    }
+  getClientsForRoom (roomName) {
+    const room = this.roomRepository.get(roomName)
+    return Array.from(room.clients.values())
+  }
 
-    /**
+  /**
      * Removes a client from the room.
-     * @param {string} roomName 
-     * @param {string} client 
+     * @param {string} roomName
+     * @param {string} client
      */
-    removeClient(roomName, client) {
-        // TODO notify client for disconnection
-    }
+  removeClient (roomName, client) {
+    // TODO notify client for disconnection
+  }
 }
-
-
